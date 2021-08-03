fd0455b5ef67de3bad4a765ba87600d48c7cd206
nullpointer606 <88355223+nullpointer606@users.noreply.github.com>
2021-08-03 21:44:07 +0900

    Update saidaikouyakusuu.sh

--- saidaikouyakusuu.sh
+++ saidaikouyakusuu.sh
@@ -2,12 +2,27 @@
 
 X=$1
 Y=$2
-r=z
 
-  while [0 -ne $Y $X -eq $Y $Y -eq $r]
-   do
-     r = $X % $Y;
-   done
-  }
+  while [ 0 -ne $Y ]
+  do
 
-echo $X;
+        r=`expr  $(($X % $Y))`
+        X=$Y
+        Y=$r
+
+        if [ $X -eq  $r ]; then
+                echo "$X　と $Y の最大公約数は見つかりません"
+                exit 0
+        fi
+
+        #echo $r
+        # echo $X
+        # echo $Y
+
+
+  done
+
+   A=$1
+   B=$2
+
+  echo "$Aと$Bの最大公約数は$Xです"
