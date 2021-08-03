458f60673390edb4811b16d96c54722646312ef8
nullpointer606 <88355223+nullpointer606@users.noreply.github.com>
2021-08-03 17:41:07 +0900

    Update saidaikouyakusuu.sh

--- saidaikouyakusuu.sh
+++ saidaikouyakusuu.sh
@@ -3,14 +3,11 @@
 X=$1
 Y=$2
 
-
-func gcd_euclid（$x, $y) {
-   while [0 -ne $Y $X -eq $Y $v -eq $r]
-    if
+  while [0 -ne $Y $X -eq $Y $v -eq $r]
    do
      r = $X % $Y;
    done
-               
   }
-  echo $X;
-}
+
+echo $X;
+
